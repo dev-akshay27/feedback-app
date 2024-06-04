2d5280da688863c9f60777781c37e02c6a581bb1
Akshay <iakshaykofficial@gmail.com>
2024-06-04 21:42:08 +0530

    initial commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae aliquet neque. Quisque quis vulputate leo, vitae sagittis tellus. Aenean laoreet odio lectus, et malesuada enim scelerisque vel. Aliquam tempor tortor metus, eu tincidunt nunc fermentum a. Quisque quam nibh, venenatis eget odio sed, commodo mattis arcu. Suspendisse viverra dolor dignissim lorem tempus dapibus. Quisque sagittis dictum sollicitudin. Sed sed lectus ut arcu eleifend faucibus vel vitae orci. Maecenas fringilla eros quis risus condimentum scelerisque.",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae aliquet neque. Quisque quis vulputate leo, vitae sagittis tellus. Aenean laoreet odio lectus, et malesuada enim scelerisque vel. Aliquam tempor tortor metus, eu tincidunt nunc fermentum a. Quisque quam nibh, venenatis eget odio sed, commodo mattis arcu. Suspendisse viverra dolor dignissim lorem tempus dapibus. Quisque sagittis dictum sollicitudin. Sed sed lectus ut arcu eleifend faucibus vel vitae orci. Maecenas fringilla eros quis risus condimentum scelerisque.",
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae aliquet neque. Quisque quis vulputate leo, vitae sagittis tellus. Aenean laoreet odio lectus, et malesuada enim scelerisque vel. Aliquam tempor tortor metus, eu tincidunt nunc fermentum a. Quisque quam nibh, venenatis eget odio sed, commodo mattis arcu. Suspendisse viverra dolor dignissim lorem tempus dapibus. Quisque sagittis dictum sollicitudin. Sed sed lectus ut arcu eleifend faucibus vel vitae orci. Maecenas fringilla eros quis risus condimentum scelerisque.",
+  },
+];
+
+export default FeedbackData;
